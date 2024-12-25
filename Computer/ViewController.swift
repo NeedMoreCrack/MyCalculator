a7b344bcd510d1797bbcb84f666c6c6245dc8592
NeedMoreCrack <qrt0zblng@gmail.com>
2024-12-25 16:30:23 +0800

    fix BUG

--- Computer/ViewController.swift
+++ Computer/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ViewController: UIViewController {
     var inputNum = ""
     var inputArray:[String] = []
+    var nowMath = ""
     @IBOutlet weak var show: UITextField!
     
     override func viewDidLoad() {
@@ -16,69 +17,90 @@ class ViewController: UIViewController {
         inputNum = ""
         inputArray = []
         show.text = "0"
+        nowMath = ""
     }
     @IBAction func clear(_ sender: Any) {
-        
+        if !inputNum.isEmpty {
+            inputNum.removeLast()
+        } else if !inputArray.isEmpty {
+            inputArray.removeLast()
+        }
+        updateDisplay()
     }
     @IBAction func zero(_ sender: Any) {
         print("按下0")
         pressNumber("0")
+        updateDisplay()
     }
     @IBAction func one(_ sender: Any) {
         print("按下1")
         pressNumber("1")
+        updateDisplay()
     }
     @IBAction func two(_ sender: Any) {
         print("按下2")
         pressNumber("2")
+        updateDisplay()
     }
     @IBAction func three(_ sender: Any) {
         print("按下3")
         pressNumber("3")
+        updateDisplay()
     }
     @IBAction func four(_ sender: Any) {
         print("按下4")
         pressNumber("4")
+        updateDisplay()
     }
     @IBAction func five(_ sender: Any) {
         print("按下5")
         pressNumber("5")
+        updateDisplay()
     }
     @IBAction func six(_ sender: Any) {
         print("按下6")
         pressNumber("6")
+        updateDisplay()
     }
     @IBAction func seven(_ sender: Any) {
         print("按下7")
         pressNumber("7")
+        updateDisplay()
     }
     @IBAction func eight(_ sender: Any) {
         print("按下8")
         pressNumber("8")
+        updateDisplay()
     }
     @IBAction func nine(_ sender: Any) {
         print("按下9")
         pressNumber("9")
+        updateDisplay()
     }
     @IBAction func point(_ sender: Any) {
         print("按下.")
         pressNumber(".")
+        updateDisplay()
     }
     @IBAction func plus(_ sender: Any) {
         print("按下+")
         pressOperator("+")
+        updateDisplay()
     }
     @IBAction func minus(_ sender: Any) {
         print("按下-")
         pressOperator("-")
+        updateDisplay()
     }
     @IBAction func multiply(_ sender: Any) {
         print("按下x")
         pressOperator("*")
+        updateDisplay()
     }
     @IBAction func dividedBy(_ sender: Any) {
         print("按下÷")
         pressOperator("/")
+        updateDisplay()
     }
     @IBAction func equal(_ sender: Any) {
         print("按下=")
@@ -90,7 +112,7 @@ class ViewController: UIViewController {
     
     func pressNumber (_ num:String) {
         inputNum += num
-        print("目前計算式：\(inputNum)")
+        print("目前計算式：\(inputArray)\n")
         show.text = inputNum
     }
     
@@ -100,6 +122,11 @@ class ViewController: UIViewController {
             inputArray.append(op)
             inputNum = ""
         }
+    }
+    
+    func updateDisplay() {
+        nowMath = inputArray.joined() + inputNum
+        show.text = nowMath.isEmpty ? "0" : nowMath
     }
     
     func finalEqual() {
@@ -131,7 +158,7 @@ class ViewController: UIViewController {
                     if number != 0 {
                         result /= number
                     } else {
-                        show.text = "錯誤：除以0"
+                        show.text = "錯誤：不能除以0"
                         inputArray = []
                         inputNum = ""
                         return
